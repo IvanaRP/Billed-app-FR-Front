c7a7ffa5eb0c54a19fe1223cc51f4501e5515c40
IvanaRP <83823351+IvanaRP@users.noreply.github.com>
2021-12-30 14:48:37 +0100

    Update NewBill.js
    console log

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -20,6 +20,7 @@ export default class NewBill {
     e.preventDefault()
   
     const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
+    console.log(file);
     const filePath = e.target.value.split(/\\/g)
     const fileName = filePath[filePath.length-1]
     const formData = new FormData()
