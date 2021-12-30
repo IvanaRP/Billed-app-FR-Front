97ef387ab9a7f2a64509e96bbe0022477f219b35
IvanaRP <83823351+IvanaRP@users.noreply.github.com>
2021-12-30 13:18:16 +0100

    bills

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -1,14 +1,31 @@
-import { screen } from "@testing-library/dom"
+// import { screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
-
+import { fireEvent, screen } from "@testing-library/dom"
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
+    test("Then I should upload validate file", () => {
       const html = NewBillUI()
       document.body.innerHTML = html
       //to-do write assertion
+
+      const inputData = {
+        files: "../assets/images/facturefreemobile.jpg"
+      }
+
+      const form = screen.getByTestId("form-new-bill")
+      const inputFile = screen.getByTestId("file")
+      fireEvent.change(inputFile, { target: { value: inputData.file } })
+
+      const validExtensions = ["jpg","jpeg","png"]
+      const regExpExtension = /(?:\.([^.]+))?$/
+      const inputFileExtension = regExpExtension.exec(inputData.files)[1]
+
+      const handleChangeFile = jest.fn(e => e.preventDefault())
+      form.addEventListener("submit", handleChangeFile)
+      fireEvent.submit(form) 
+      expect(validExtensions).toContain(inputFileExtension)
     })
   })
 })
