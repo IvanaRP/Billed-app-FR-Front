9777b3e65eaf2333fc0c331aef1ac4eba9f16512
IvanaRP <83823351+IvanaRP@users.noreply.github.com>
2022-01-12 16:09:01 +0100

    Update NewBill.js
    progress test

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -1,18 +1,45 @@
-import { screen } from "@testing-library/dom"
+
+
+
+
+
+
+import { fireEvent, screen } from "@testing-library/dom";
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
 
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then I should upload validate file", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
+    // test("Then ...", () => {
+    //   const html = NewBillUI()
+    //   document.body.innerHTML = html
+    //   //to-do write assertion
 
+    // })
+    test("Then I should upload validated file", () => {
+      const html = NewBillUI()
+      document.body.innerHTML = html 
+
+      const inputData = {
+        files: "../assets/images/avataaars_Girl2.png"
+      }
+      
+      const form = screen.getByTestId("form-new-bill")
+      const inputFile = screen.getByTestId("file")
+      fireEvent.change(inputFile, { target: { value: inputData.file } })
+      const validExtensions = ["jpg","jpeg","png"]
+      const regExpExtension = /(?:\.([^.]+))?$/
+      const inputFileExtension = regExpExtension.exec(inputData.files)[1]
+      const handleChangeFile = jest.fn(e => e.preventDefault())
+      form.addEventListener("submit", handleChangeFile)
+      fireEvent.submit(form) 
+      expect(validExtensions).toContain(inputFileExtension)
     })
   })
 })
+
+
 
 
 
